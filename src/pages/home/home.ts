5846451d28041ac8e2abcb53505d90db86dddece
Jeffrey Hernandez <jeffredh@usc.edu>
2017-11-04 12:23:19 -0700

    imported login-page to home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from "../login/login";
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
+  loginPage = LoginPage;
+	constructor(public navCtrl: NavController) {
+    
+	}
 
 }
