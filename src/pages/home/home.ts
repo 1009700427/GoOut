1e53e38262431102aa8852f2b886e524a99ac12f
Sina Karachiani <karachia@users.noreply.github.com>
2017-11-04 12:12:09 -0700

    Sign up page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { SignUpPage } from "../sign-up/sign-up";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	SignUp = SignUpPage;
 
   constructor(public navCtrl: NavController) {
 
