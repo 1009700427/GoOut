57ae05f972223b54ebde0c464a5ebb6ed1daf40d
Sina Karachiani <karachia@users.noreply.github.com>
2017-11-04 12:43:28 -0700

    added buttons to sign up

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SignUpPage } from "../sign-up/sign-up";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
