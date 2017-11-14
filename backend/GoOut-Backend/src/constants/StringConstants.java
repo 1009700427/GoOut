c83a563119fc21be8fd1a57cdadd7919d5a22197
Jeffrey Hernandez <jeffredh@usc.edu>
2017-11-14 14:37:42 -0800

    added stringconstants class to keep the output message uniform and just need to change one variable

---
+++ backend/GoOut-Backend/src/constants/StringConstants.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class StringConstants {
+	public static final String INVALID_USERNAME = "Invalid Username";
+	public static final String TAKEN_USERNAME = "Username has already been taken";
+	public static final String INVALID_PASSWORD = "Invalid password";
+	
+	//parameters for the httprequest.
+	//update these as need be to match the ionic backend.
+	public static final String PARAM_USERNAME = "username";
+	public static final String PARAM_PASSWORD = "password";
+	public static final String PARAM_FIRST_NAME = "firstname";
+	public static final String PARAM_LAST_NAME = "lastname";
+	public static final String PARAM_CONFIRM_PASSWORD ="confirmPassword";
+}
