9a9b27f33c969e3cb93224d6bd4b64caef93910f
Jeffrey Hernandez <jeffredh@usc.edu>
2017-11-18 20:06:20 -0800

    keeping my copy of the login.html/ts files

--- ionic-app/src/pages/login/login.ts
+++ ionic-app/src/pages/login/login.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {Component} from '@angular/core';
 import { SignUpPage } from "../sign-up/sign-up";
 import { MainPage } from "../main/main";
@@ -46,48 +45,5 @@ export class LoginPage{
 		// }	
 	}
 
-=======
-import {Component} from '@angular/core';
-import { SignUpPage } from "../sign-up/sign-up";
-import { MainPage } from "../main/main";
-import { eventPage } from "../event-detail/event-detail";
-import { addEventPage } from '../add-event/add-event';
-import { userEventPage } from "../user-event/user-event";
-import {Platform, ActionSheetController} from 'ionic-angular';
 
-@Component({
-	selector: 'home-page',
-	templateUrl: "login.html"
-
-})
-
-export class LoginPage{
-	SignUpPage = SignUpPage;
-	MainPage = MainPage;
-	eventPage = eventPage;
-	userEventPage = userEventPage;
-	addEventPage = addEventPage;
-	constructor(
-		public Platform: Platform, 
-		public actionsheetCtrl: ActionSheetController
-	){}
-	
-	openMenu(){
-		let actionsheet = this.actionsheetCtrl.create({
-			title: 'albums',
-			cssClass: 'action-sheets-basic-page', 
-			buttons:[
-			{
-				text: 'Delete',
-				role: 'destructive',
-				icon:!this.Platform.is('ios') ? 'heart-outline': null,
-				handler: () => {
-					console.log('Favorite Clicked');
-				}
-			}
-			]
-		});
-		actionsheet.present();
-	}
->>>>>>> master
 }
