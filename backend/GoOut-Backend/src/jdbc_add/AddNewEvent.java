f6536d632b072c7787110d3d857e75e6d858a0fe
Steven Xu <siyuanx@usc.edu>
2017-11-19 00:06:14 -0800

    AddNewEvent

--- backend/GoOut-Backend/src/jdbc_add/AddNewEvent.java
+++ backend/GoOut-Backend/src/jdbc_add/AddNewEvent.java
@@ -1,8 +1,17 @@
+/*
+ * 
+ * startTime and endTime strings must be in "hh:mm:ss" format in order to be successfully parsed 
+ * 
+ */
 package jdbc_add;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,15 +40,31 @@ public class AddNewEvent extends HttpServlet {
 		String eventName = request.getParameter("eventName");
 		String userIDStr = request.getParameter("userID");
 		String location = request.getParameter("location");
-		int month = Integer.parseInt(request.getParameter("month")); 
-		int day = Integer.parseInt(request.getParameter("day")); 
+		int month = 0; 
+		try{
+			month = Integer.parseInt(request.getParameter("month")); 
+		}catch(NumberFormatException nfe){
+			System.out.println("nfe: "+nfe.getMessage());
+			month = 0; 
+		}
+		int day = 0; 
+		try{
+			day = Integer.parseInt(request.getParameter("day")); 
+		}catch(NumberFormatException nfe){
+			System.out.println("nfe: "+nfe.getMessage());
+			day = 0; 
+		}
 		String startTime = request.getParameter("startTime");
-		String endTime = request.getParameter(request.getParameter("endTime")); 
-		boolean privateEvent = request.getParameter("privateEvent") != null;
+		String endTime = request.getParameter("endTime"); 
+		boolean privateEvent = false;
 		// check null 
+		// if true, directs back to AddNewEventForm
 		if(eventName==null || eventName=="" || userIDStr==null || userIDStr==""){
-			request.setAttribute("errmsg", "Invalid username and password.");
-			response.sendRedirect("");
+			 response.setContentType("text/html");  
+			 PrintWriter out = response.getWriter(); 
+			 out.println("Event name, user ID and event privacy cannot be null");
+			response.sendRedirect("AddNewEventForm.html");
+			return; 
 		}
 		int userID = Integer.parseInt(userIDStr); 
 		Connection conn = null;
@@ -53,7 +78,39 @@ public class AddNewEvent extends HttpServlet {
 			conn = DriverManager.getConnection("jdbc:mysql://cs201.cll9sbto0nla.us-west-1.rds.amazonaws.com/GoOutDB?user=master&password=masterpassword&useSSL=false");
 			st = conn.createStatement();
 			System.out.println("connected");
+			psUpdate = conn.prepareStatement("INSERT INTO Events(eventName, userID, location, month, day, startTime, endTime, privateEvent)"
+					+"VALUES(?,?,?,?,?,?,?,?)");
+			psUpdate.setString(1, eventName);
+			psUpdate.setInt(2, userID);
+			psUpdate.setString(3, location);
+			psUpdate.setInt(4, month);
+			psUpdate.setInt(5, day);
 			
+			java.sql.Time start = getSQLTime(startTime);
+			System.out.println("start: "+ start);
+			psUpdate.setTime(6, start);
+			System.out.println("end: "+endTime);
+			java.sql.Time end = getSQLTime(endTime);
+			psUpdate.setTime(7, end);
+			psUpdate.setBoolean(8, privateEvent);
+			// executes the script 
+			psUpdate.executeUpdate();
+			System.out.println("Script executed!");
+			
+			psDisplay = conn.prepareStatement("SELECT * FROM Events WHERE eventName = ?");
+			psDisplay.setString(1, eventName);
+			rs = psDisplay.executeQuery();
+			while(rs.next()){
+				String eventName_ = rs.getString("eventName");
+				int userID_ = rs.getInt("userID");
+				String location_ = rs.getString("location");
+				request.setAttribute("userID", userID_);
+				request.setAttribute("eventName", eventName_);
+				request.setAttribute("location", location_);
+			}
+			
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/AddNewEventResults.jsp");
+			dispatcher.forward(request, response);
 		}catch(SQLException sqle) {
 			System.out.println("sqle: " + sqle.getMessage());
 		}catch (ClassNotFoundException cnfe) {
@@ -74,5 +131,17 @@ public class AddNewEvent extends HttpServlet {
 				System.out.println("sqle closing stuff: " + sqle.getMessage());
 			}
 		}
+	}
+	// gets the correct time object for SQL 
+	private java.sql.Time getSQLTime(String timeStr){
+		java.sql.Time ppstime = null; 
+		try{
+			SimpleDateFormat textFormat = new SimpleDateFormat("hh:mm:ss");
+			java.util.Date d1 =(java.util.Date)textFormat.parse(timeStr);
+			ppstime = new java.sql.Time(d1.getTime()); 
+		}catch(ParseException pe){
+			System.out.println("ParseException: "+pe.getMessage());
+		}
+		return ppstime;
 	}
 }
