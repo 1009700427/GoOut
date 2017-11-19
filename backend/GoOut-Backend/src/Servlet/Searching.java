1698171cc04c7fae5f975469e7864803e2bb702b
Steven Xu <siyuanx@usc.edu>
2017-11-18 21:26:33 -0800

    resolve-conflict

--- backend/GoOut-Backend/src/Servlet/Searching.java
+++ backend/GoOut-Backend/src/Servlet/Searching.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Servlet;
 
 import javax.servlet.ServletException;
@@ -43,53 +42,3 @@ public class Searching extends HttpServlet {
 	}
 
 }
-=======
-package Servlet;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import objects.JDBCDriver;
-
-/**
- * Servlet implementation class Searching
- */
-@WebServlet("/Searching")
-public class Searching extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		//JDBCDriver.connect();
-//		String rep = request.getParameter("sent");
-//		response.getWriter().println("i received your msg");
-//		response.getWriter().flush();
-//		response.getWriter().close();
-//		
-//	}
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException{
-		//JDBCDriver.connect();
-		
-		
-		//TESTING THE CONNECTION REMOVE WHEN DONE 
-//		try {
-//			String rep = request.getParameter("sent");
-//			if(rep != null) {
-//				response.getWriter().println("i received your msg: " + rep);
-//				response.getWriter().flush();
-//				response.getWriter().close();
-//			}
-//			
-//		}catch(IOException ioe) {
-//			ioe.printStackTrace();
-//		}
-		
-	}
-
-}
->>>>>>> b89b793b62c1deb51046f48a741d713348016084
