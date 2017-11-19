89381f57c1650eaaca453ef3a35a10895199d9f5
Steven Xu <siyuanx@usc.edu>
2017-11-18 21:05:26 -0800

    master-steve

--- backend/GoOut-Backend/src/Servlet/Searching.java
+++ backend/GoOut-Backend/src/Servlet/Searching.java
@@ -1,14 +1,10 @@
 package Servlet;
 
-import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import objects.JDBCDriver;
 
 /**
  * Servlet implementation class Searching
