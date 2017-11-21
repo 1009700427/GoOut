2e690255e5e670d4d45ecbc2ba64208af11e79fb
Jeffrey Hernandez <jeffredh@usc.edu>
2017-11-20 18:59:56 -0800

    fixed bug

--- ionic-app/src/pages/find-events/find-events.ts
+++ ionic-app/src/pages/find-events/find-events.ts
@@ -49,26 +49,32 @@ export class FindEventsPage {
           console.log(req.responseText);
           console.log("eventFound");
           var split = req.responseText.split("\n");
+
           var eventID = page.arraytify(split[0]);
-          var eventTitle = page.arraytify(split[1]);
-          var usernames = page.arraytify(split[2]);
-          //console.log("userIDS" + userIDs);
-          var eventLocation = page.arraytify(split[3]);
-          var eventMonths = page.arraytify(split[4]);
-          var eventDays = page.arraytify(split[5]);
-          var eventStart = page.arraytify(split[6]);
-          var eventEnd = page.arraytify(split[7]); 
-          var descrip = page.arraytify(split[8]);
+          if(eventID.length > 0  && eventID[0] != ''){
+            console.log(eventID.length);
+            var eventTitle = page.arraytify(split[1]);
+            var usernames = page.arraytify(split[2]);
+            //console.log("userIDS" + userIDs);
+            var eventLocation = page.arraytify(split[3]);
+            var eventMonths = page.arraytify(split[4]);
+            var eventDays = page.arraytify(split[5]);
+            var eventStart = page.arraytify(split[6]);
+            var eventEnd = page.arraytify(split[7]); 
+            var descrip = page.arraytify(split[8]);
+            for(var i = 0; i < eventTitle.length; i++){
+            //let user = page.findUser(userIDs[i]);
+            page.addEvent(eventID[i],eventTitle[i], usernames[i], eventLocation[i], eventMonths[i],eventDays[i], eventStart[i], eventEnd[i], descrip[i]);
+            }
+          }
+          else{
+            console.log('notfound');
+          }
+          
           // for(var i = 0; i < userIDs.length; i++){
           //   console.log(page.findUser(userIDs[i]));
           // }
-          for(var i = 0; i < eventTitle.length; i++){
-            //let user = page.findUser(userIDs[i]);
-            page.addEvent(eventID[i],eventTitle[i], usernames[i], eventLocation[i], eventMonths[i],eventDays[i], eventStart[i], eventEnd[i], descrip[i]);
-          }
-        }
-        else{
-          console.log('notfound');
+          
         }
       }
     }
