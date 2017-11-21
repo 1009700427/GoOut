66fba0b3336e4dc56e1d20bec048eb89fc9754b5
Jeffrey Hernandez <jeffredh@usc.edu>
2017-11-20 22:05:47 -0800

    working

--- ionic-app/src/pages/add-event/add-event.ts
+++ ionic-app/src/pages/add-event/add-event.ts
@@ -59,7 +59,7 @@ export class addEventPage {
       var dateList = date.split("-");
       var timeList = time.split(":");
 
-
+      
 
       var message = dateList[2]+"/"+dateList[1]+"/2017"+timeList[0]+"/"+timeList[1]+"/00/1/"+title;
 
